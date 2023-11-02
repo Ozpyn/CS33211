905df5754cf9486b1c56bb2be3efd496f54b07cc
Ozpyn <gabrielmingle@gmail.com>
2023-11-02 12:51:43 -0400

    Update producer-consumer.c
    Include default libraries, along with semaphore and pthread libraries.
    
    Also define Number of items, table size, number of producers, number of consumers.

--- producer-consumer.c
+++ producer-consumer.c
@@ -1,3 +1,19 @@
+/*
+    Gabriel Mingle
+    CS 33211: Operating Systems
+    Dr. Qiang Guan
+    Programming Assignment #1: Producer Consumer Problem
+*/
+
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define MaxItems 5    // To limit the run-time of the program
+#define TableSize 2   // Size of the Table, Assignment defines the max is 2
+#define NumProducer 1 // Produce More??
+#define NumConsumer 1 // Eat more??
 
 // Producer generates items and places into table
 // Consumer eats items
