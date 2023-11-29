d5f0fc404eb91e1ed55157ad1ca6606538e024f6
Ozpyn <gabrielmingle@gmail.com>
2023-11-29 11:57:32 -0500

    finished bankers-algorithm.cpp

--- BankersAlgorithm/bankers-algorithm.cpp
+++ BankersAlgorithm/bankers-algorithm.cpp
@@ -3,6 +3,7 @@
     CS 33211: Operating Systems
     Dr. Qiang Guan
     Programming Assignment #2: Banker's Algorithm Problem
+    Due: December 1, 2023
 */
 #include <iostream>
 #include <fstream>
@@ -10,65 +11,117 @@ using namespace std;
 
 int main()
 {
-    int n = 5; // Number of processes
-    int m = 3; // Number of resources
+    int row = 5; // Number of processes
+    int col = 3; // Number of resources
 
     ifstream allocFile("alloc.txt");
-    int alloc[n][m];
+    int alloc[row][col];
     if (!allocFile)
     {
         cout << "Unable to open alloc.txt";
     }
-
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < row; i++)
     {
-        {
-            for (int j = 0; j < m; j++)
-                allocFile >> alloc[i][j]; // Import alloc.txt
-        }
+        for (int j = 0; j < col; j++)
+            allocFile >> alloc[i][j]; // Import alloc.txt
     }
 
     ifstream maxFile("max.txt");
-    int max[n][m];
+    int max[row][col];
     if (!maxFile)
     {
         cout << "Unable to open max.txt";
     }
-
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < row; i++)
     {
         {
-            for (int j = 0; j < m; j++)
+            for (int j = 0; j < col; j++)
                 maxFile >> max[i][j]; // import max.txt
         }
     }
 
     ifstream availFile("avail.txt");
-    int avail[m];
+    int avail[col];
     if (!availFile)
     {
         cout << "Unable to open avail.txt";
     }
-    {
-        for (int j = 0; j < m; j++)
-            availFile >> avail[j]; // Import avail.txt
-    }
+    for (int j = 0; j < col; j++)
+        availFile >> avail[j]; // Import avail.txt
 
-    int need[n][m];
-    for (int a = 0; a < n; a++)
+    int need[row][col];
+    for (int a = 0; a < row; a++)
     {
-        for (int b = 0; b < m; b++)
+        for (int b = 0; b < col; b++)
         {
             need[a][b] = max[a][b] - alloc[a][b]; // Make need array to determine needed values
         };
     };
 
-    // Check if sequence is safe or not
-    // Is not safe
-    cout << "The given sequence is not safe";
+    bool s[row];       // Verify each operation is safe
+    int sequence[row]; // Store order of operations
+    int index = 0;     // Current index in sequence
+    for (int k = 0; k < row; k++)
+    {
+        s[k] = false; // Populate safe array with false
+    };
+    for (int k = 0; k < row; k++)
+    {
+        for (int i = 0; i < row; i++)
+        {
+            if (s[i] == 0)
+            {
+                int safe = true;
+                for (int j = 0; j < col; j++)
+                {
+                    if (need[i][j] > avail[j])
+                    {
+                        safe = false; // Not Safe, needs more than can be provided
+                        break;
+                    }
+                }
+                if (safe == true)
+                {
+                    // cout << "Available: ";
+                    sequence[index++] = i;
+                    for (int y = 0; y < col; y++)
+                    {
+                        avail[y] += alloc[i][y]; // Reclaim allocated resources
+                        // cout << avail[y] << '\t';
+                    }
+                    // cout << endl;
+                    s[i] = true;
+                }
+            }
+        }
+    };
 
-    // Is Safe
-    cout << "Following is the Safe Sequence" << endl;
+    bool isSafe = true; // Bool because only 2 possible answers
+
+    // Check if sequence is safe or not
+    for (int i = 0; i < row; i++)
+    {
+        if (s[i] == false)
+        {
+            isSafe = false;
+            // Not safe
+            break;
+        }
+    }
+
+    if (isSafe == true)
+    {
+        // Is Safe
+        cout << "Following is the Safe Sequence" << endl;
+        for (int i = 0; i < row - 1; i++)
+            cout << " P" << sequence[i] << " ->"; // Print Correct Order
+        cout << " P" << sequence[row - 1] << endl;
+    }
+    else
+    {
+        // Is not safe
+        cout << "The given sequence is not safe";
+    }
 
     return 0;
 }
